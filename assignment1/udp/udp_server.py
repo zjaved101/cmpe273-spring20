55461a9b3a0ac7742f6a168a1a02c8f0849326dd
Zan Javed <{ID}+{username}@users.noreply.github.com>
2020-02-12 22:39:18 -0800

    doesn't do packet loss handeling yet

--- assignment1/udp/udp_server.py
+++ assignment1/udp/udp_server.py
@@ -1,5 +1,5 @@
 import socket
-
+import asyncio
 
 UDP_IP = '127.0.0.1'
 UDP_PORT = 4000
@@ -17,5 +17,24 @@ def listen_forever():
         # reply back to the client
         s.sendto(MESSAGE.encode(), ip)
 
+async def handleClient(udpSocket, ip, data):
+    sequence = 0
+    print("{}: {}".format(ip, data.decode(encoding="utf-8").strip()))
+    udpSocket.sendto(MESSAGE.encode(), ip)
 
-listen_forever()
+async def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(("", UDP_PORT))
+
+    while True:
+        print('Waiting for clients...')
+        data, ip = s.recvfrom(BUFFER_SIZE)
+        print('Handling client: {}'.format(ip))
+        await handleClient(s, ip, data)
+        print('Handled client: {}'.format(ip))
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt as e:
+        print(e)
