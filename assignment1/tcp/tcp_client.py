97f047ba046c9861d63337a0126efff363c1563a
Zan Javed <{ID}+{username}@users.noreply.github.com>
2020-02-14 16:11:39 -0800

    client will stay alive until interrupted

--- assignment1/tcp/tcp_client.py
+++ assignment1/tcp/tcp_client.py
@@ -20,22 +20,21 @@ def get_client_id():
     id = input("Enter client id:")
     return id
 
-
-# send(get_client_id())
-
 async def main():
-    # id = get_client_id()
-    # reader, writer = await asyncio.open_connection(TCP_IP, TCP_PORT)
+    print("Press Ctrl + c to quit client")
+    id = get_client_id()
+    try:
+        while True:
+            reader, writer = await asyncio.open_connection(TCP_IP, TCP_PORT)
+            message = "%s:ping" % (id)
+            writer.write(message.encode())
+            data = await reader.read(BUFFER_SIZE)
+            print("Received data:%s" % data.decode())
+            writer.close()
+    except KeyboardInterrupt:
+        print("Client closed...")
 
-    for id in range(0, 100):
-        reader, writer = await asyncio.open_connection(TCP_IP, TCP_PORT)
-        message = "%s:ping" % (id)
-        writer.write(message.encode())
-        data = await reader.read(BUFFER_SIZE)
-        print("Received data:%s" % data.decode())
-
-        writer.close()
-
+    writer.close()
 
 if __name__ == "__main__":
     asyncio.run(main())
