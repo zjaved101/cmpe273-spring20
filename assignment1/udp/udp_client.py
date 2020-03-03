368b4d0901974830b2e04348061813f6f279cbbc
Zan Javed <{ID}+{username}@users.noreply.github.com>
2020-03-02 22:11:44 -0800

    reads file bytes and sends to client while waiting for acks

--- assignment1/udp/udp_client.py
+++ assignment1/udp/udp_client.py
@@ -7,6 +7,7 @@ UDP_IP = '127.0.0.1'
 UDP_PORT = 4000
 BUFFER_SIZE = 1024
 MESSAGE = "ping"
+FILE = "upload.txt"
 
 def send(id=0):
     try:
@@ -23,7 +24,8 @@ def get_client_id():
     return id
 
 def sendMessage(udpSocket, id, sequence, message):
-    udpSocket.sendto(f"{id}:{sequence}:{message}".encode(), (UDP_IP, UDP_PORT))
+    # udpSocket.sendto(f"{id}:{sequence}:{message}".encode(), (UDP_IP, UDP_PORT))
+    udpSocket.sendto(f"{id}:{sequence}:{message}", (UDP_IP, UDP_PORT))
 
 def receiveMessage(udpSocket):
     data, ip = udpSocket.recvfrom(BUFFER_SIZE)
@@ -34,39 +36,78 @@ def openFile():
         return f.readlines()
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("id", help="client id")
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("id", default="1",help="client id")
+    # args = parser.parse_args()
 
     try:
-        file = openFile()
-        # id = get_client_id()
-        id = args.id
-        sequence = 0
-        # sequence = SequenceCounter()
+        # file = openFile()
+        # # id = get_client_id()
+        # id = args.id
+        # sequence = 0
+        # # sequence = SequenceCounter()
+        # s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # s.settimeout(1)
+
+        # while sequence < len(file):
+        #     try:
+        #         sendMessage(s, id, sequence, file[sequence].strip())
+        #         data, ip = receiveMessage(s)
+        #         split = data.decode().split(':')
+        #         # if sequence == int(data.decode()):
+        #         print("id received: {}".format(split[0]))
+        #         if sequence == int(split[1]) and id == split[0]:
+        #             print("Received acknowledgement for sequence: {}".format(sequence))
+        #             sequence += 1
+        #         else:
+        #             print("Did not receive acknowledgement for sequence: {}".format(sequence))
+        #             print("Resending packet...")
+        #     except socket.timeout as e:
+        #         print(e)
+        id = 1
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.settimeout(1)
+        
+        # send initial message for server connection
+        s.sendto(f"{id}:{'-2'}".encode(), (UDP_IP, UDP_PORT))
+        data, ip = receiveMessage(s)
+        if data.decode() == "0":
+            print("Connected to the server.")
+        else:
+            print("Cannot connect to the server. Exitting...")
+            return
+        
+        with open(FILE, "rb") as f:
+            print("Starting a file (%s) upload..." % FILE)
+            data = f.read(BUFFER_SIZE)
 
-        while sequence < len(file):
-            try:
-                sendMessage(s, id, sequence, file[sequence].strip())
+            sequence = 1
+            while data:
+                sendMessage(s, id, sequence, data)
                 data, ip = receiveMessage(s)
                 split = data.decode().split(':')
-                # if sequence == int(data.decode()):
-                print("id received: {}".format(split[0]))
-                if sequence == int(split[1]) and id == split[0]:
-                    print("Received acknowledgement for sequence: {}".format(sequence))
+                if sequence == int(split[1]) and id == int(split[0]):
+                    print("Received ack({}) from the server.".format(sequence))
                     sequence += 1
+                    data = f.read(BUFFER_SIZE)
                 else:
-                    print("Did not receive acknowledgement for sequence: {}".format(sequence))
-                    print("Resending packet...")
-            except socket.timeout as e:
-                print(e)
+                    print(data.decode())
+                    print("Did not receive ack({}) from the server. Resending last packet...".format(sequence))
+                
+        s.sendto(f"{id}:{'-1'}".encode(), (UDP_IP, UDP_PORT))
+        data, ip = receiveMessage(s)
+
+        if data.decode() == "1":
+            print("File upload successfully completed.")
+        else:
+            print("File upload not successfully completed.")
 
     except IOError as e:
         print(e)
     except socket.error:
         print("Error! {}".format(socket.error))
+    except socket.timeout as e:
+        print(e)
     except KeyboardInterrupt as e:
         print("Keyboard Interrupt detected...")
 
