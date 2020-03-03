ca9e6d13d10e8183f8280c51ac1a8a8decbc4a7d
Zan Javed <{ID}+{username}@users.noreply.github.com>
2020-03-02 20:54:30 -0800

    saves file data if sequence number is in order

--- assignment1/udp/udp_server.py
+++ assignment1/udp/udp_server.py
@@ -5,25 +5,39 @@ UDP_IP = '127.0.0.1'
 UDP_PORT = 4000
 BUFFER_SIZE = 1024
 MESSAGE = "pong"
+FILE_DATA = {}
 
-def listen_forever():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind(("", UDP_PORT))
+# def listen_forever():
+#     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#     s.bind(("", UDP_PORT))
 
-    while True:
-        # get the data sent to us
-        data, ip = s.recvfrom(BUFFER_SIZE)
-        print("{}: {}".format(ip, data.decode(encoding="utf-8").strip()))
-        # reply back to the client
-        s.sendto(MESSAGE.encode(), ip)
+#     while True:
+#         # get the data sent to us
+#         data, ip = s.recvfrom(BUFFER_SIZE)
+#         print("{}: {}".format(ip, data.decode(encoding="utf-8").strip()))
+#         # reply back to the client
+#         s.sendto(MESSAGE.encode(), ip)
 
 async def handleClient(udpSocket, ip, data):
-    print("{}: {}".format(ip, data.decode(encoding="utf-8").strip()))
+    # print("{}: {}".format(ip, data.decode(encoding="utf-8").strip()))
+    # split = data.decode().split(':')
+    # udpSocket.sendto(':'.join(split[0:2]).encode(), ip)
+
     split = data.decode().split(':')
+    if split[0] not in FILE_DATA:
+        print("Accepting file upload from client: %s" % split[0])
+        FILE_DATA[split[0]] = {"data" : [], "sequence" : -1}
+    
+    # if sequence is the next sequence, accept incoming file data
+    if int(split[1]) == FILE_DATA["sequence"] + 1:
+        FILE_DATA[split[0]]["data"].append(split[2])
+        FILE_DATA[split[0]]["sequence"] = int(split[1])
+    
     udpSocket.sendto(':'.join(split[0:2]).encode(), ip)
 
 async def main():
     print("Press Ctrl + c to exit this server...")
+    print("Server started at port %s" % UDP_PORT)
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind(("", UDP_PORT))
 
