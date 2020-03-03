df73dbb1a266c91be8ddf0fba777c6b019fd1f76
Zan Javed <{ID}+{username}@users.noreply.github.com>
2020-03-03 10:27:51 -0800

    reads data from clients and writes the bytes to file once transfer is complete

--- assignment1/udp/udp_server.py
+++ assignment1/udp/udp_server.py
@@ -4,28 +4,36 @@ import os
 
 UDP_IP = '127.0.0.1'
 UDP_PORT = 4000
-BUFFER_SIZE = 1024
+# BUFFER_SIZE = 1024
+BUFFER_SIZE = 4096
 MESSAGE = "pong"
 FILE_DATA = {}
+SEQUENCE = 0
 FILE = "upload.txt"
 
 def writeDataToFile(id):
     if not os.path.exists("output"):
         os.mkdir("output")
 
+    # print(FILE_DATA[id])
     with open("output/%s" % FILE, 'wb') as f:
         for data in FILE_DATA[id]["data"]:
-            f.write(data)
+            f.write(data.encode())
+            # f.write(data)
 
 async def handleClient(udpSocket, ip, data):
     # print("{}: {}".format(ip, data.decode(encoding="utf-8").strip()))
     # split = data.decode().split(':')
     # udpSocket.sendto(':'.join(split[0:2]).encode(), ip)
 
-    split = data.decode().split(':')
+    # print(data)
+    # print(type(data))
+    # if isinstance(data, bytes):
+    split = data.decode('utf-8').split('.')
     id = split[0]
     # acknowledgement to client that server is alive
     if split[1] == "-2":
+        print("Acknowleding client %s" % id)
         udpSocket.sendto("0".encode(), ip)
         return
 
@@ -34,8 +42,13 @@ async def handleClient(udpSocket, ip, data):
         print("Upload successfully completed.")
         udpSocket.sendto("1".encode(), ip)
         writeDataToFile(id)
+        del FILE_DATA[id]
         return
 
+    # if isinstance(data, bytearray):
+        # print("BYTEARRAY")
+    # split = data[0].decode().split('+')
+    # id = split[0]
     if id not in FILE_DATA:
         print("Accepting file upload from client: %s" % id)
         FILE_DATA[id] = {"data" : [], "sequence" : 0}
