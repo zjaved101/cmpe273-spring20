f6bbb2c6759846110c6bfba00f4c7bd7a3fc10d5
Zan Javed <{ID}+{username}@users.noreply.github.com>
2020-03-02 17:07:06 -0800

    added commandline arguments for id,delay,ping

--- assignment1/tcp/tcp_client.py
+++ assignment1/tcp/tcp_client.py
@@ -1,32 +1,31 @@
 import socket
 import asyncio
-
+import argparse
+import time
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5000
 BUFFER_SIZE = 1024
 MESSAGE = "ping"
 
-def send(id=0):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((TCP_IP, TCP_PORT))
-    s.send(f"{id}:{MESSAGE}".encode())
-    data = s.recv(BUFFER_SIZE)
-    s.close()
-    print("received data:", data.decode())
-
-
-def get_client_id():
-    id = input("Enter client id:")
-    return id
-
 async def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('id', type=int, help='Client ID')
+    parser.add_argument('delay', type=int, help='Delay between printing messages')
+    parser.add_argument('ping', type=int, help='Number of times to send the message ping to server')
+    args = parser.parse_args()
+
     print("Press Ctrl + c to quit client")
-    id = get_client_id()
     try:
-        while True:
+        for i in range(0, args.ping):
             reader, writer = await asyncio.open_connection(TCP_IP, TCP_PORT)
-            message = "%s:ping" % (id)
+            message = "%s:%s" % (args.id, MESSAGE)
+
+            # delay message only if user provided seconds greater than 1
+            if args.delay > 1:
+                time.sleep(args.delay)
+
+            print("Sending data: %s" % message)
             writer.write(message.encode())
             data = await reader.read(BUFFER_SIZE)
             print("Received data:%s" % data.decode())
