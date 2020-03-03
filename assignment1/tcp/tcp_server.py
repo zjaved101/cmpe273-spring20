ada4259543ab64f6166e5c5df2929dd49e054f61
Zan Javed <{ID}+{username}@users.noreply.github.com>
2020-03-02 17:08:36 -0800

    cleaned up comments

--- assignment1/tcp/tcp_server.py
+++ assignment1/tcp/tcp_server.py
@@ -5,37 +5,6 @@ TCP_IP = '127.0.0.1'
 TCP_PORT = 5000
 BUFFER_SIZE = 1024
 
-# def listen_forever():
-#     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     s.bind((TCP_IP, TCP_PORT))
-#     s.listen(1)
-
-#     conn, addr = s.accept()
-#     print(f'Connection address:{addr}')
-
-#     while True:
-#         data = conn.recv(BUFFER_SIZE)
-#         if not data: 
-#             print('No data received.')
-#             break
-#         print(f"received data:{data.decode()}")
-#         conn.send("pong".encode())
-
-#     conn.close()
-
-# def listen():
-#     '''
-#         Listens for connections coming from clients
-#     '''
-#     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     s.bind((TCP_IP, TCP_PORT))
-#     s.listen(1)
-
-#     conn, addr = s.accept()
-#     print(f'Connection address:{addr}')
-
-#     return conn, addr
-
 async def handleClient(reader, writer):
     data = await reader.read(BUFFER_SIZE)
     message = data.decode()
